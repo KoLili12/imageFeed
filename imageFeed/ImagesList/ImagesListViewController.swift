3b080874aac0057cfee1a38b4b890f8f537f640c
NickZhirnoff <nickzhirnoff@gmail.com>
2024-12-18 20:35:36 +0300

    Итог 9 спринт

--- imageFeed/ImagesList/ImagesListViewController.swift
+++ imageFeed/ImagesList/ImagesListViewController.swift
@@ -11,6 +11,7 @@ final class ImagesListViewController: UIViewController {
     
     // MARK: - Private properties
     
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     private let currentDate = Date()
     private lazy var dateFormatter: DateFormatter = {
@@ -23,9 +24,26 @@ final class ImagesListViewController: UIViewController {
     // MARK: - @IBOutlet properties
 
     @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -60,7 +78,7 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // TODO: - Добавить логику при нажатии на ячейку
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else { return 0 }
--- imageFeed/Profile/ProfileViewController.swift
+++ imageFeed/Profile/ProfileViewController.swift
@@ -9,18 +9,101 @@ import UIKit
 
 final class ProfileViewController:UIViewController {
     
-    // MARK: - @IBOutlet properties
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let avatar = avatarImage()
+        let name = nameLabel()
+        let id = identifierLabel()
+        let description = descriptionLabel()
+        let exit = exitButton()
+        
+        view.addSubview(avatar)
+        view.addSubview(name)
+        view.addSubview(id)
+        view.addSubview(description)
+        view.addSubview(exit)
+        
+        NSLayoutConstraint.activate([
+            avatar.topAnchor.constraint(equalTo: view.topAnchor, constant: 76),
+            avatar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            name.topAnchor.constraint(equalTo: avatar.bottomAnchor, constant: 8),
+            name.leadingAnchor.constraint(equalTo: avatar.leadingAnchor),
+            id.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 8),
+            id.leadingAnchor.constraint(equalTo: avatar.leadingAnchor),
+            description.topAnchor.constraint(equalTo: id.bottomAnchor, constant: 8),
+            description.leadingAnchor.constraint(equalTo: avatar.leadingAnchor),
+            exit.centerYAnchor.constraint(equalTo: avatar.centerYAnchor),
+            exit.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
+        ])
+    }
     
-    @IBOutlet private var nameLabel: UILabel!
+    // MARK: - Private function
     
-    @IBOutlet private var loginNameLabel: UILabel!
+    @objc private func didTapExitButton() {
+        let alert = UIAlertController(title: "Пока, пока!", message: "Уверены что хотите выйти?", preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Да", style: .default) { _ in
+            
+        }
+        
+        let noAction = UIAlertAction(title: "Нет", style: .cancel) { _ in
+            
+        }
+
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
     
-    @IBOutlet private var logoutButton: UIButton!
+    private func avatarImage() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "WomanAvatar")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalToConstant: 70),
+            imageView.widthAnchor.constraint(equalToConstant: 70)
+            ])
+        return imageView
+    }
     
-    @IBOutlet private var descriptionLabel: UILabel!
+    private func nameLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        label.textColor = .ypWhiteIOS
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
     
-    // MARK: - @IBAction properties
+    private func identifierLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "@eka_novikova"
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .ypGrayIOS
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
     
-    @IBAction private func didTapLogoutButton(_ sender: Any) {
+    private func descriptionLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "Hello, world!"
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .ypWhiteIOS
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+    
+    private func exitButton() -> UIButton {
+        let button = UIButton.systemButton(with: UIImage(named: "Exit") ?? UIImage(), target: self, action: #selector(self.didTapExitButton))
+        button.tintColor = .ypRedIOS
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.heightAnchor.constraint(equalToConstant: 44),
+            button.widthAnchor.constraint(equalToConstant: 44),
+        ])
+        return button
     }
 }
